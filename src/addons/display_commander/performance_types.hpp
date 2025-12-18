b8d4233c5da11d72842eb4fad00909838b87dd3d
Piotr Mikulski <piotr.mikulski@gmail.coM>
2025-12-18 08:09:01 -0800

    Revert "Add adjusting process priority"
    This reverts commit aecc9ace5d7ddb7d063fe3336c1129928891d094.

--- src/addons/display_commander/performance_types.hpp
+++ src/addons/display_commander/performance_types.hpp
@@ -12,13 +12,3 @@ enum class FrameTimeMode : std::uint8_t {
     kFrameBegin = 1,     // Frame Begin-Frame Begin (only record FrameBegin calls)
     kDisplayTiming = 2   // Display Timing (record when frames are actually displayed based on GPU completion)
 };
-
-// Process priority class enum
-enum class ProcessPriority : std::uint8_t {
-    kDefault = 0,              // NORMAL_PRIORITY_CLASS (default, no change)
-    kBelowNormal = 1,           // BELOW_NORMAL_PRIORITY_CLASS
-    kNormal = 2,                // NORMAL_PRIORITY_CLASS
-    kAboveNormal = 3,           // ABOVE_NORMAL_PRIORITY_CLASS
-    kHigh = 4,                  // HIGH_PRIORITY_CLASS
-    kRealtime = 5              // REALTIME_PRIORITY_CLASS (use with caution)
-};

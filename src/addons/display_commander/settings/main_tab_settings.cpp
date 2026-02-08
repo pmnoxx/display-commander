7c27bd92fa5dc218592fcbdd9b2502cb2c00e1f7
Piotr Mikulski <piotr.mikulski@gmail.coM>
2026-02-08 00:48:46 -0800

    Make sim start only native pacing default

--- src/addons/display_commander/settings/main_tab_settings.cpp
+++ src/addons/display_commander/settings/main_tab_settings.cpp
@@ -64,7 +64,7 @@ MainTabSettings::MainTabSettings()
           "DisplayCommander"),  // Default to 100% Display / 0% Input (current behavior)
       pcl_stats_enabled("pcl_stats_enabled", false, "DisplayCommander"),
       experimental_fg_native_fps_limiter("experimental_fg_native_fps_limiter", true, "DisplayCommander"),
-      native_pacing_sim_start_only("native_pacing_sim_start_only", false, "DisplayCommander"),
+      native_pacing_sim_start_only("native_pacing_sim_start_only", true, "DisplayCommander"),
       experimental_safe_mode_fps_limiter("experimental_safe_mode_fps_limiter", false, "DisplayCommander"),
       force_vsync_on("force_vsync_on", s_force_vsync_on, s_force_vsync_on.load(), "DisplayCommander"),
       force_vsync_off("force_vsync_off", s_force_vsync_off, s_force_vsync_off.load(), "DisplayCommander"),

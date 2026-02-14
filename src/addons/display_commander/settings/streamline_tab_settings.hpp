431b61cf44dc85b468abb948bc81aa42c2c1be13
Piotr Mikulski <piotr.mikulski@gmail.coM>
2026-02-13 22:35:10 -0800

    Remove Experimental features from UI

--- src/addons/display_commander/settings/streamline_tab_settings.hpp
+++ src/addons/display_commander/settings/streamline_tab_settings.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
-#include "../ui/new_ui/settings_wrapper.hpp"
 #include <atomic>
 #include <string>
+#include "../ui/new_ui/settings_wrapper.hpp"
 
 namespace settings {
 
 // DLSS Override Settings
 class StreamlineTabSettings {
-public:
+   public:
     StreamlineTabSettings();
     ~StreamlineTabSettings() = default;
 
@@ -21,12 +21,13 @@ public:
     // DLSS Override Settings
     ui::new_ui::BoolSetting dlss_override_enabled;
     ui::new_ui::StringSetting dlss_override_folder;
-    ui::new_ui::BoolSettingRef dlss_override_dlss;        // nvngx_dlss.dll
-    ui::new_ui::BoolSettingRef dlss_override_dlss_fg;     // nvngx_dlssd.dll (DLSS-FG)
-    ui::new_ui::BoolSettingRef dlss_override_dlss_rr;     // nvngx_dlssg.dll (DLSS-RR)
+    ui::new_ui::StringSetting dlss_override_subfolder;  // "" = Default Folder, else subfolder name under dlss_override
+    ui::new_ui::BoolSettingRef dlss_override_dlss;      // nvngx_dlss.dll
+    ui::new_ui::BoolSettingRef dlss_override_dlss_fg;  // nvngx_dlssd.dll (DLSS-FG)
+    ui::new_ui::BoolSettingRef dlss_override_dlss_rr;  // nvngx_dlssg.dll (DLSS-RR)
 
-private:
+   private:
     std::vector<ui::new_ui::SettingBase*> all_settings_;
 };
 
-} // namespace settings
+}  // namespace settings

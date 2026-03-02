070c850baa27e2bf0498e4784cb7de9c5c800217
Piotr Mikulski <piotr.mikulski@gmail.coM>
2026-03-01 17:18:10 -0800

    Bump to v0.12.203: fix default colorspace for AutoHDR to auto

--- src/addons/display_commander/settings/main_tab_settings.cpp
+++ src/addons/display_commander/settings/main_tab_settings.cpp
@@ -193,7 +193,7 @@ MainTabSettings::MainTabSettings()
       max_anisotropy("max_anisotropy", 0, 0, 16, "DisplayCommander"),
       force_mipmap_lod_bias("force_mipmap_lod_bias", 0.0f, -5.0f, 5.0f, "DisplayCommander"),
       brightness_percent("brightness_percent", s_brightness_percent, 100.0f, 0.0f, 200.0f, "DisplayCommander"),
-      brightness_colorspace("brightness_colorspace", s_brightness_colorspace, 1,
+      brightness_colorspace("brightness_colorspace2", s_brightness_colorspace, 0,
                             {"Auto", "scRGB(default)", "HDR10", "sRGB", "Gamma 2.2", "None"}, "DisplayCommander"),
       gamma_value("gamma_value", s_gamma_value, 1.0f, 0.5f, 2.0f, "DisplayCommander"),
       contrast_value("contrast_value", s_contrast_value, 1.0f, 0.0f, 2.0f, "DisplayCommander"),

229817ad50d1601e7e5e6e6a296297e5da62241f
Piotr Mikulski <piotr.mikulski@gmail.coM>
2026-03-13 21:41:25 -0700

    README: add .dll to proxy list names

---
+++ scripts/try_all_proxies.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+"""
+Copy dc64.dll to all Wine/Proton proxy DLL names that don't already exist in the
+target directory. Use this to discover which libraries a game loads: place dc64.dll
+(Display Commander 64-bit proxy) in the game folder, run this script, then run the game.
+Whichever DLL names get loaded will use dc64.dll (you can add logging in the proxy to see).
+
+Wine dlls list: https://github.com/wine-mirror/wine/tree/master/dlls
+Excludes non-.dll outputs: .tlb, .cpl, .msstyles, .sys, .drv16, .dll16.
+
+Usage:
+  python copy_dc64_to_wine_proxies.py [target_dir]
+  python copy_dc64_to_wine_proxies.py "C:\Program Files (x86)\Steam\steamapps\common\STEINS;GATE"
+
+If target_dir is omitted, uses current directory.
+Source DLL is target_dir/dc64.dll (must exist).
+"""
+
+from __future__ import print_function
+
+import os
+import shutil
+import sys
+
+# Try to load list from same directory as this script (when run from repo)
+_SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+if _SCRIPT_DIR not in sys.path:
+    sys.path.insert(0, _SCRIPT_DIR)
+try:
+    from wine_proxy_dll_list import WINE_PROXY_DLLS
+except ImportError:
+    # Embedded list when script is copied alone (e.g. to game dir)
+    WINE_PROXY_DLLS = """
+acledit.dll aclui.dll activeds.dll actxprxy.dll adsldp.dll adsldpc.dll advapi32.dll
+advpack.dll amsi.dll amstream.dll apisetschema.dll apphelp.dll appxdeploymentclient.dll
+atl.dll atl100.dll atl110.dll atl80.dll atl90.dll atlthunk.dll atmlib.dll authz.dll
+avicap32.dll avifil32.dll avrt.dll bcp47langs.dll bcrypt.dll bcryptprimitives.dll
+bluetoothapis.dll browseui.dll cabinet.dll capi2032.dll cards.dll cdosys.dll cfgmgr32.dll
+chakra.dll cldapi.dll clusapi.dll colorcnv.dll combase.dll comcat.dll comctl32.dll
+comctl32_v6.dll comdlg32.dll coml2.dll compstui.dll comsvcs.dll concrt140.dll connect.dll
+coremessaging.dll credui.dll crtdll.dll crypt32.dll cryptbase.dll cryptdlg.dll cryptdll.dll
+cryptext.dll cryptnet.dll cryptowinrt.dll cryptsp.dll cryptui.dll cryptxml.dll ctapi32.dll
+ctl3d32.dll d2d1.dll d3d10.dll d3d10_1.dll d3d10core.dll d3d11.dll d3d12.dll d3d12core.dll
+d3d8.dll d3d8thk.dll d3d9.dll d3dcompiler_33.dll d3dcompiler_34.dll d3dcompiler_35.dll
+d3dcompiler_36.dll d3dcompiler_37.dll d3dcompiler_38.dll d3dcompiler_39.dll d3dcompiler_40.dll
+d3dcompiler_41.dll d3dcompiler_42.dll d3dcompiler_43.dll d3dcompiler_46.dll d3dcompiler_47.dll
+d3dim.dll d3dim700.dll d3drm.dll d3dx10_33.dll d3dx10_34.dll d3dx10_35.dll d3dx10_36.dll
+d3dx10_37.dll d3dx10_38.dll d3dx10_39.dll d3dx10_40.dll d3dx10_41.dll d3dx10_42.dll d3dx10_43.dll
+d3dx11_42.dll d3dx11_43.dll d3dx9_24.dll d3dx9_25.dll d3dx9_26.dll d3dx9_27.dll d3dx9_28.dll
+d3dx9_29.dll d3dx9_30.dll d3dx9_31.dll d3dx9_32.dll d3dx9_33.dll d3dx9_34.dll d3dx9_35.dll
+d3dx9_36.dll d3dx9_37.dll d3dx9_38.dll d3dx9_39.dll d3dx9_40.dll d3dx9_41.dll d3dx9_42.dll d3dx9_43.dll
+d3dxof.dll dataexchange.dll davclnt.dll dbgeng.dll dbghelp.dll dciman32.dll dcomp.dll
+ddraw.dll ddrawex.dll devenum.dll dhcpcsvc.dll dhcpcsvc6.dll dhtmled.ocx.dll diasymreader.dll
+difxapi.dll dinput.dll dinput8.dll directmanipulation.dll dispex.dll dmband.dll dmcompos.dll
+dmime.dll dmloader.dll dmscript.dll dmstyle.dll dmsynth.dll dmusic.dll dmusic32.dll dnsapi.dll
+dplay.dll dplayx.dll dpnaddr.dll dpnet.dll dpnhpast.dll dpnhupnp.dll dpnlobby.dll dpvoice.dll
+dpwsockx.dll drmclien.dll dsdmo.dll dsound.dll dsquery.dll dssenh.dll dsuiext.dll dswave.dll
+dwmapi.dll dwrite.dll dx8vb.dll dxcore.dll dxdiagn.dll dxgi.dll dxtrans.dll dxva2.dll
+esent.dll evr.dll explorerframe.dll faultrep.dll feclient.dll fltlib.dll fntcache.dll fontsub.dll
+fusion.dll fwpuclnt.dll gameinput.dll gameux.dll gamingtcui.dll gdi.exe16.dll gdi32.dll
+gdiplus.dll geolocation.dll glu32.dll gphoto2.ds.dll gpkcsp.dll graphicscapture.dll hal.dll
+hhctrl.ocx.dll hid.dll hlink.dll hnetcfg.dll hrtfapo.dll httpapi.dll hvsimanagementapi.dll
+ia2comproxy.dll iccvid.dll icmp.dll icmui.dll ieframe.dll ieproxy.dll iertutil.dll ifsmgr.vxd.dll
+imaadp32.acm.dll imagehlp.dll imm32.dll inetcomm.dll inetmib1.dll infosoft.dll initpki.dll
+inkobj.dll inseng.dll iphlpapi.dll iprop.dll ir50_32.dll itircl.dll itss.dll jscript.dll
+jsproxy.dll kerberos.dll kernel32.dll kernelbase.dll krnl386.exe16.dll ksproxy.ax.dll ksuser.dll
+ktmw32.dll l3codeca.acm.dll l3codecx.ax.dll loadperf.dll localspl.dll localui.dll lz32.dll
+magnification.dll mapi32.dll mapistub.dll mciavi32.dll mcicda.dll mciqtz32.dll mciseq.dll
+mciwave.dll mf.dll mf3216.dll mfasfsrcsnk.dll mferror.dll mfh264enc.dll mfmediaengine.dll
+mfmp4srcsnk.dll mfplat.dll mfplay.dll mfreadwrite.dll mfsrcsnk.dll mgmtapi.dll midimap.dll
+mlang.dll mmcndmgr.dll mmdevapi.dll mmdevldr.vxd.dll monodebg.vxd.dll mp3dmod.dll mpr.dll
+mprapi.dll msacm32.drv.dll msacm32.dll msado15.dll msadp32.acm.dll msasn1.dll msauddecmft.dll
+mscat32.dll mscms.dll mscoree.dll mscorwks.dll msctf.dll msctfmonitor.dll msctfp.dll msdaps.dll
+msdasql.dll msdelta.dll msdmo.dll msdrm.dll msftedit.dll msg711.acm.dll msgsm32.acm.dll
+mshtml.dll msi.dll msident.dll msimg32.dll msimsg.dll msimtf.dll msisip.dll msisys.ocx.dll
+msls31.dll msmpeg2vdec.dll msnet32.dll mspatcha.dll msports.dll msrle32.dll msscript.ocx.dll
+mssign32.dll mssip32.dll mstask.dll msttsengine.dll msv1_0.dll msvcirt.dll msvcm80.dll
+msvcm90.dll msvcp100.dll msvcp110.dll msvcp120.dll msvcp120_app.dll msvcp140.dll msvcp140_1.dll
+msvcp140_2.dll msvcp140_atomic_wait.dll msvcp140_codecvt_ids.dll msvcp60.dll msvcp70.dll
+msvcp71.dll msvcp80.dll msvcp90.dll msvcp_win.dll msvcr100.dll msvcr110.dll msvcr120.dll
+msvcr120_app.dll msvcr70.dll msvcr71.dll msvcr80.dll msvcr90.dll msvcrt.dll msvcrt20.dll
+msvcrt40.dll msvcrtd.dll msvdsp.dll msvfw32.dll msvidc32.dll msvproc.dll mswsock.dll
+msxml.dll msxml2.dll msxml3.dll msxml4.dll msxml6.dll mtxdm.dll ncrypt.dll nddeapi.dll
+netapi32.dll netcfgx.dll netprofm.dll netutils.dll newdev.dll ninput.dll normaliz.dll
+npmshtml.dll npptools.dll nsi.dll ntdll.dll ntdsapi.dll ntoskrnl.exe.dll ntprint.dll objsel.dll
+odbc32.dll odbcbcp.dll odbccp32.dll odbccu32.dll ole32.dll oleacc.dll oleaut32.dll olecli32.dll
+oledb32.dll oledlg.dll olepro32.dll olesvr32.dll olethk32.dll opcservices.dll opencl.dll
+opengl32.dll packager.dll pdh.dll photometadatahandler.dll pidgen.dll powrprof.dll printui.dll
+prntvpt.dll profapi.dll propsys.dll psapi.dll pstorec.dll pwrshplugin.dll qasf.dll qcap.dll
+qdvd.dll qedit.dll qmgr.dll qmgrprxy.dll quartz.dll query.dll qwave.dll rasapi32.dll rasdlg.dll
+regapi.dll resampledmo.dll resutils.dll riched20.dll riched32.dll rometadata.dll rpcrt4.dll
+rsabase.dll rsaenh.dll rstrtmgr.dll rtutils.dll rtworkq.dll samlib.dll sane.ds.dll sapi.dll
+sas.dll scarddlg.dll scardsvr.dll sccbase.dll schannel.dll schedsvc.dll scrobj.dll scrrun.dll
+sechost.dll secur32.dll security.dll sensapi.dll serialui.dll setupapi.dll sfc.dll sfc_os.dll
+shcore.dll shdoclc.dll shdocvw.dll shell32.dll shfolder.dll shlwapi.dll slbcsp.dll slc.dll
+snmpapi.dll softpub.dll spoolss.dll sppc.dll srclient.dll srvcli.dll srvsvc.dll sspicli.dll
+sti.dll strmdll.dll svrapi.dll sxs.dll t2embed.dll tapi32.dll taskschd.dll tbs.dll tdh.dll
+threadpoolwinrt.dll traffic.dll twain_32.dll twaindsm.dll twinapi.appcore.dll tzres.dll
+ucrtbase.dll uianimation.dll uiautomationcore.dll uiribbon.dll unicows.dll updspapi.dll
+url.dll urlmon.dll user.exe16.dll user32.dll userenv.dll usp10.dll utildll.dll uxtheme.dll
+vbscript.dll vccorlib140.dll vcomp.dll vcomp100.dll vcomp110.dll vcomp120.dll vcomp140.dll
+vcomp90.dll vcruntime140.dll vcruntime140_1.dll vdhcp.vxd.dll vdmdbg.dll version.dll vga.dll
+vidreszr.dll virtdisk.dll vmm.vxd.dll vnbt.vxd.dll vnetbios.vxd.dll vssapi.dll vtdapi.vxd.dll
+vulkan-1.dll vwin32.vxd.dll w32skrnl.dll wbemdisp.dll wbemprox.dll wdscore.dll webservices.dll
+websocket.dll wer.dll wevtapi.dll wevtsvc.dll wiaservc.dll wimgapi.dll win32u.dll winbio.dll
+winbrand.dll windows.applicationmodel.dll windows.devices.bluetooth.dll windows.devices.enumeration.dll
+windows.devices.usb.dll windows.gaming.input.dll windows.gaming.ui.gamebar.dll windows.globalization.dll
+windows.graphics.dll windows.media.devices.dll windows.media.mediacontrol.dll
+windows.media.playback.backgroundmediaplayer.dll windows.media.playback.mediaplayer.dll
+windows.media.speech.dll windows.media.dll windows.networking.connectivity.dll windows.networking.hostname.dll
+windows.networking.dll windows.perception.stub.dll windows.security.authentication.onlineid.dll
+windows.security.credentials.ui.userconsentverifier.dll windows.storage.applicationdata.dll windows.storage.dll
+windows.system.profile.systemid.dll windows.system.profile.systemmanufacturers.dll windows.ui.core.textinput.dll
+windows.ui.xaml.dll windows.ui.dll windows.web.dll windowscodecs.dll windowscodecsext.dll
+winealsa.drv.dll wineandroid.drv.dll winecoreaudio.drv.dll wined3d.dll winedmo.dll winegstreamer.dll
+winemac.drv.dll winemapi.dll wineoss.drv.dll wineps.drv.dll winepulse.drv.dll winevulkan.dll
+winewayland.drv.dll winex11.drv.dll wing32.dll winhttp.dll wininet.dll winmm.dll winnls32.dll
+winprint.dll winscard.dll winspool.drv.dll winsta.dll wintab32.dll wintrust.dll wintypes.dll
+winusb.dll wlanapi.dll wlanui.dll wldap32.dll wldp.dll wmadmod.dll wmasf.dll wmi.dll
+wminet_utils.dll wmiutils.dll wmp.dll wmphoto.dll wmvcore.dll wmvdecod.dll wnaspi32.dll
+wofutil.dll wow32.dll wow64.dll wow64cpu.dll wow64win.dll wpc.dll wpcap.dll ws2_32.dll
+wsdapi.dll wshom.ocx.dll wsnmp32.dll wsock32.dll wtsapi32.dll wuapi.dll wuaueng.dll
+x3daudio1_0.dll x3daudio1_1.dll x3daudio1_2.dll x3daudio1_3.dll x3daudio1_4.dll x3daudio1_5.dll
+x3daudio1_6.dll x3daudio1_7.dll xactengine2_0.dll xactengine2_4.dll xactengine2_7.dll xactengine2_9.dll
+xactengine3_0.dll xactengine3_1.dll xactengine3_2.dll xactengine3_3.dll xactengine3_4.dll
+xactengine3_5.dll xactengine3_6.dll xactengine3_7.dll xapofx1_1.dll xapofx1_2.dll xapofx1_3.dll
+xapofx1_4.dll xapofx1_5.dll xaudio2_0.dll xaudio2_1.dll xaudio2_2.dll xaudio2_3.dll xaudio2_4.dll
+xaudio2_5.dll xaudio2_6.dll xaudio2_7.dll xaudio2_8.dll xaudio2_9.dll xinput1_1.dll xinput1_2.dll
+xinput1_3.dll xinput1_4.dll xinput9_1_0.dll xinputuap.dll xmllite.dll xolehlp.dll xpsprint.dll
+xpssvcs.dll xtajit64.dll
+""".strip().split()
+
+
+def main():
+    if len(sys.argv) > 1:
+        target_dir = os.path.abspath(sys.argv[1])
+    else:
+        target_dir = os.getcwd()
+
+    source_dll = os.path.join(target_dir, "zzz_display_commander.addon64")
+    if not os.path.isfile(source_dll):
+        print("Error: dc64.dll not found in", target_dir, file=sys.stderr)
+        print("Place dc64.dll in the target directory first.", file=sys.stderr)
+        sys.exit(1)
+
+    copied = []
+    skipped = []
+    for name in WINE_PROXY_DLLS:
+        dest = os.path.join(target_dir, name)
+        if os.path.isfile(dest):
+            skipped.append(name)
+            continue
+        try:
+            shutil.copy2(source_dll, dest)
+            copied.append(name)
+        except OSError as e:
+            print("Warning: could not copy to {}: {}".format(name, e), file=sys.stderr)
+
+    print("Target directory:", target_dir)
+    print("Source:", source_dll)
+    print("Copied dc64.dll to {} proxy name(s).".format(len(copied)))
+    print("Skipped {} (already exist).".format(len(skipped)))
+    if copied:
+        print("\nCreated (first 30):", ", ".join(copied[:30]))
+        if len(copied) > 30:
+            print("... and {} more.".format(len(copied) - 30))
+    if skipped and len(skipped) <= 20:
+        print("\nSkipped (already present):", ", ".join(skipped))
+    elif skipped:
+        print("\nSkipped {} already-present DLL(s).".format(len(skipped)))
+
+
+if __name__ == "__main__":
+    main()

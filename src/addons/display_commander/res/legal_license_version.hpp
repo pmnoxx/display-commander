3199faedfccc38994a0765146347ff70111a930e
Piotr Mikulski <piotr.mikulski@gmail.coM>
2026-03-22 20:27:54 -0700

    added missing MIT license

--- src/addons/display_commander/res/legal_license_version.hpp
+++ src/addons/display_commander/res/legal_license_version.hpp
@@ -3,6 +3,6 @@
 namespace display_commander::legal {
 
 // Bump when LICENSE / THIRD_PARTY_NOTICES change requires re-acceptance in the UI.
-constexpr int kLicensePresentationVersion = 1001;
+constexpr int kLicensePresentationVersion = 1002;
 
 }  // namespace display_commander::legal
